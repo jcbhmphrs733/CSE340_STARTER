43ff015f7c07f31472b9d9a62390dc7904578c3f
Jacob Humphreys <jacob_humphreys@icloud.com>
2025-05-09 14:11:17 -0600

    Update

--- server.js
+++ server.js
@@ -1,19 +1,29 @@
-/* ******************************************
- * This server.js file is the primary file of the 
- * application. It is used to control the project.
- *******************************************/
 /* ***********************
  * Require Statements
  *************************/
 const express = require("express")
 const env = require("dotenv").config()
-const app = express()
+const expressLayouts = require("express-ejs-layouts")
 const static = require("./routes/static")
+const app = express()
+
+/***********************
+  * View Engine and Templates
+  *************************/
+app.set("view engine", "ejs")
+app.use(expressLayouts)
+app.set("layout", "layouts/layout")
+
+
 
 /* ***********************
  * Routes
  *************************/
-app.use(static)
+app.use(require("./routes/static"))
+//Index route
+app.get("/", (req, res) => {
+  res.render("index", {title: "Home"})
+})
 
 /* ***********************
  * Local Server Information
