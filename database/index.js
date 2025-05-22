455cc007223949d8cf08e36faf9dfcc82c7bbbda
Jacob Humphreys <jacob_humphreys@icloud.com>
2025-05-21 21:59:31 -0600

    ${pnpm_config_message:-Update}

--- database/index.js
+++ database/index.js
@@ -6,32 +6,31 @@ require("dotenv").config()
  * But will cause problems in production environment
  * If - else will make determination which to use
  * *************** */
-let pool
+let pool;
 if (process.env.NODE_ENV == "development") {
   pool = new Pool({
     connectionString: process.env.DATABASE_URL,
     ssl: {
       rejectUnauthorized: false,
     },
-})
+  });
+} else {
+  pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+  });
+}
 
 // Added for troubleshooting queries
 // during development
 module.exports = {
   async query(text, params) {
     try {
-      const res = await pool.query(text, params)
-    //   console.log("executed query", { text })
-      return res
+      const res = await pool.query(text, params);
+      //   console.log("executed query", { text })
+      return res;
     } catch (error) {
-      console.error("error in query", { text })
-      throw error
+      console.error("error in query", { text });
+      throw error;
     }
   },
-}
-} else {
-  pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-  })
-  module.exports = pool
-}
+};
