b792280a9f791aa679287962011df6064bb6744c
Jacob Humphreys <jacob_humphreys@icloud.com>
2025-05-23 11:43:21 -0600

    ${pnpm_config_message:-Update}

--- models/inventory-model.js
+++ models/inventory-model.js
@@ -4,7 +4,7 @@ const pool = require("../database/")
  *  Get all classification data
  * ************************** */
 async function getClassifications(){
-  return await pool.query("SELECT * FROM public.classification ORDER BY classification_name;")
+  return await pool.query("SELECT * FROM public.classification ORDER BY classification_name")
 }
 
 /* ***************************
@@ -19,7 +19,7 @@ async function getInventoryByClassificationId(classification_id) {
       WHERE i.classification_id = $1`,
       [classification_id]
     )
-    return data.rows
+    // return data.rows
   } catch (error) {
     console.error("getclassificationsbyid error " + error)
   }
